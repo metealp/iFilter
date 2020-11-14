7b03997c1cf555f838246412ee800f9d0a002943
Mete Alp Kızılçay <kizilcaymetealp@gmail.com>
2020-11-14 15:15:59 +0300

    Initial Commit

---
+++ iFilter/iFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFilterApp.swift
+//  iFilter
+//
+//  Created by Mete Alp Kizilcay on 14.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct iFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
